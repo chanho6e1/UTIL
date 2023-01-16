e5b450fefcceb7d20c1d2c9d38a0db304db995f0
박찬희 <shine7065@naver.com>
2023-01-16 22:55:23 +0900

    feat:be/github-login

--- server/src/main/java/com/youtil/server/oauth/info/OAuth2UserInfoFactory.java
+++ server/src/main/java/com/youtil/server/oauth/info/OAuth2UserInfoFactory.java
@@ -1,10 +1,7 @@
 package com.youtil.server.oauth.info;
 
 import com.youtil.server.oauth.entity.ProviderType;
-import com.youtil.server.oauth.info.impl.FacebookOAuth2UserInfo;
-import com.youtil.server.oauth.info.impl.GoogleOAuth2UserInfo;
-import com.youtil.server.oauth.info.impl.KakaoOAuth2UserInfo;
-import com.youtil.server.oauth.info.impl.NaverOAuth2UserInfo;
+import com.youtil.server.oauth.info.impl.*;
 
 import java.util.Map;
 
@@ -12,6 +9,7 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
         switch (providerType) {
             case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
+            case GITHUB: return new GithubOAuth2UserInfo(attributes);
             case FACEBOOK: return new FacebookOAuth2UserInfo(attributes);
             case NAVER: return new NaverOAuth2UserInfo(attributes);
             case KAKAO: return new KakaoOAuth2UserInfo(attributes);
