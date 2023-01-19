cf6498b04a9dbbfe0854914eac460054cddc9b10
“gms9424” <gms9424@naver.com>
2023-01-19 12:48:09 +0900

    feat: be/server_folder_delete

--- server/src/main/java/com/youtil/server/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.youtil.server.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
