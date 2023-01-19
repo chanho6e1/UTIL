c5a30f14f8c4003138e6644c291fc858dd0457a3
Dongju Kim <jook1356@gmail.com>
2023-01-19 15:59:14 +0900

    commit

--- util/src/components/Plan/CalendarBar.js
+++ util/src/components/Plan/CalendarBar.js
@@ -1,24 +1,48 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from './CalendarBar.module.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { modifyPlanSliceActions } from '../../redux/planSlice'
 import DraggableDateSelector from "./DraggableDateSelector";
+import Swipe from "react-easy-swipe";
+
+
+
+
 
 const CalendarBar = (props) => {
   const plans = useSelector(state => state.planSlice.plans)
   const monthRange = Array(props.endRange.getMonth() - props.startRange.getMonth() + 1).fill()
   const planRange = [0, 1, 2, 3]
   const planGridRef = Array(planRange.length).fill(useRef([]), 0, planRange.length)
+  const containerRef = useRef()
+  const [placedMonth, setPlacedMonth] = useState()
   
+
+
+
+
+  // const onSwipeMove = (position = { x: null }) => {
+  //   const element = document.elementsFromPoint(x, y);
+  //   // console.log(idx)
+  //   // setPositionx(() => position.x)
+  // }
+
+
+
+  const mouseOverHandler = (idx, event) => {
+    setPlacedMonth(() => idx)
+  }
+
+
   
 
   const gridPerPlans = (rowIdx) => {
     const exec = monthRange.map((el, idx) => {
       return (
-        <div key={`month-plan-bar-${idx}`}>
+        <div onMouseOver={mouseOverHandler.bind(this, idx)} key={`month-plan-bar-${idx}`}>
           <div ref={el => (planGridRef[rowIdx].current[idx] = el)} id={`month-${props.startRange.getMonth() + idx + 1}`} className={styles['month-plan-bar']}>
             
-          </div>
+          </div> 
         </div>
       )
     })
@@ -38,10 +62,12 @@ const CalendarBar = (props) => {
   const totalPlansGrid = planRange.map((el, idx) => {
     const columns = gridPerPlans(idx)
     return (
-      <div className={styles['month-bar-container']} key={`month-bar-container-${idx}`}>
-          {columns}
-          <DraggableDateSelector idx={0} period={plans[0].period} startDate={plans[0].startDate} endDate={plans[0].endDate} planGridRef={planGridRef} />
-      </div>
+
+        <div ref={containerRef} className={styles['month-bar-container']} key={`month-bar-container-${idx}`}>
+            {columns}
+            <DraggableDateSelector idx={idx} period={plans[idx].period} startDate={plans[idx].startDate} endDate={plans[idx].endDate} planGridRef={planGridRef} />
+        </div>
+
     )
   })
 
@@ -52,7 +78,7 @@ const CalendarBar = (props) => {
 
 
   return (
-    <div onClick={() => {console.log(planGridRef)}} className={styles['date-range-wrapper']}>
+    <div onClick={() => {console.log(planGridRef)}} id="date-range" className={styles['date-range-wrapper']}>
       <div className={styles['month-bar-container']}>
         {monthTitleGrid}
       </div>
