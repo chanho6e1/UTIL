0ed9aeef7fa83370e8b77301c80dafb5112968ab
“gms9424” <gms9424@naver.com>
2023-01-21 01:53:34 +0900

    chore: be/folder_delete

--- server/src/main/java/com/youtil/server/server/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.youtil.server.server.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
