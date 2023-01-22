f028828d8d715382cb44c51619bec6fe76363b69
“gms9424” <gms9424@naver.com>
2023-01-23 00:52:02 +0900

    feat: be/cursor_pagination_test

--- server/src/main/java/com/youtil/server/repository/post/PostCommentQueryRepository.java
+++ server/src/main/java/com/youtil/server/repository/post/PostCommentQueryRepository.java
@@ -4,39 +4,27 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.youtil.server.domain.post.PostComment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 import static com.youtil.server.domain.post.QPost.post;
 import static com.youtil.server.domain.post.QPostComment.postComment;
+import static com.youtil.server.domain.post.QPostLike.postLike;
 
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Repository
-public interface PostCommentQueryRepository {
+public class PostCommentQueryRepository {
 
-//    private final JPAQueryFactory jpaQueryFactory;
-//        public List<PostComment> findByUserComment(Long userId){
-//        return jpaQueryFactory
-//                .select(postComment)
-//                .distinct()
-//                .from(postComment)
-//                .innerJoin(postComment.user)
-//                .where(postComment.user.userId.eq(userId))
-//                .orderBy(findCriteria("date")).fetch();
-//    }
+    private final JPAQueryFactory jpaQueryFactory;
 
-    private OrderSpecifier<?> findCriteria(String criteria){ //정렬 조건
-        if(criteria.contains("date")){
-            return post.createdDate.desc();
-        } else if(criteria.contains("like")){
-            return post.postLikeList.postLikeList.size().desc();
-        } else if(criteria.contains("view")){
-            return post.views.desc();
-        } else if(criteria == null){
-            return post.createdDate.desc();
-        }
-
-        return post.createdDate.desc();
+    public List<PostComment> findPostList(Long postId, PageRequest pageRequest) {
+        return jpaQueryFactory.select(postComment).from(postComment).distinct()
+                .where(postComment.post.postId.eq(postId))
+                .orderBy(postComment.createdDate.desc())
+                .offset(pageRequest.getOffset()).limit(pageRequest.getPageSize())
+                .fetch();
     }
 }
--- server/src/main/java/com/youtil/server/repository/post/PostCommentRepository.java
+++ server/src/main/java/com/youtil/server/repository/post/PostCommentRepository.java
@@ -1,17 +1,45 @@
 package com.youtil.server.repository.post;
 
+import com.youtil.server.domain.post.Post;
 import com.youtil.server.domain.post.PostComment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
 
     @Transactional
     @Modifying
-    @Query("delete from PostComment c where c.post.id = :postId")
+    @Query("delete from PostComment c where c.post.postId = :postId")
     void deleteByPostId(@Param("postId") Long postId);
+
+    @Query("select c from PostComment c where c.commentId = :commnetId")
+    Optional<PostComment> findComment(@Param("commnetId") Long commnetId);
+
+    @Transactional
+    @Modifying
+    @Query("delete from PostComment c where c.commentId = :commentId")
+    void deleteByCommentId(Long commentId);
+
+//    List<PostComment> findAllByOrderByIdDesc(Pageable page); //최초 조회
+//
+//    List<PostComment> findByIdLessThanOrderByIdDesc(Long id, Pageable page);
+//
+//    Boolean existsByIdLessThan(Long id);
+
+//    @Query("SELECT c FROM PostComment c WHERE c.post.postId = :postId AND c.parentId is null and ((:cop = '<' and  c.commentId < :cursor) or (:cop = '>' and  c.commentId > :cursor))")
+    @Query("SELECT c FROM PostComment c WHERE c.post.postId = :postId and ((:cop = '<' and  c.commentId < :cursor) or (:cop = '>' and  c.commentId > :cursor))")
+    Slice<PostComment> findCommentListByCursor(@Param("postId")Long postId, @Param("cursor") Long cursor, @Param("cop")String cop, Pageable pageable);
+
+    @Query("SELECT c FROM PostComment c, User u WHERE c.post.postId = :postId AND c.parentId is not null and ((:cop = '<' and  c.commentId < :cursor) or (:cop = '>' and  c.commentId > :cursor))")
+    Slice<PostComment> findCommentChildrenListByCursor(@Param("postId")Long postId, @Param("cursor") Long cursor, @Param("cop")String cop, Pageable pageable);
+
 }
 
