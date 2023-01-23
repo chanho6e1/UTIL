04824f06465c0fca71d0544908fbccd8b87b71b6
박찬희 <gn05211@gmail.com>
2023-01-24 02:35:08 +0900

    feat:be/user nickName Check, userInfo Update

--- server/src/main/java/com/youtil/server/service/UserService.java
+++ server/src/main/java/com/youtil/server/service/UserService.java
@@ -1,9 +1,11 @@
 package com.youtil.server.service;
 
 import com.youtil.server.domain.user.User;
+import com.youtil.server.dto.user.UserUpdateRequest;
 import com.youtil.server.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +15,12 @@ public class UserService {
     public User getUser(Long userId) {
         return userRepository.findByUserId(userId);
     }
+    @Transactional
+    public User updateUser(Long userId, UserUpdateRequest request){
+        User originUser = getUser(userId);
+        originUser.update(request);
+        return originUser;
+    }
+
+    public boolean checkNickName(String nickName){ return userRepository.existsByNickName(nickName); }
 }
