de90a4c3c8a8176e43fd95d6c3be578cb0e492e5
Dongju Kim <jook1356@gmail.com>
2023-01-30 17:50:57 +0900

    feat/fe/goals_axios_get

--- util/src/constants/index.js
+++ util/src/constants/index.js
@@ -1,17 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { userAuthSliceActions } from '../redux/userAuthSlice'
-
-import React, {useEffect} from 'react'
-
-export const Token = () => {
-    const userAuth = useSelector(state => state.userAuthSlice.userAuth)
-    const token = `Bearer ${userAuth.token}`
-    return token
-}
-
-
 export const API_BASE_URL = 'http://i8d210.p.ssafy.io:8081';
 export const ACCESS_TOKEN = 'accessToken';
+
+export const TOKEN = () => {
+    const token = localStorage.getItem('accessToken')
+    return `Bearer ${token}`
+}
 
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 
