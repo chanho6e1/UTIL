e106ed09669b3a117b6a1000ed55da9e11a47187
박찬희 <shine7065@naver.com>
2023-02-01 20:25:34 +0900

    feat:be/todo-Chan getPeriod error fix

--- server/src/main/java/com/youtil/server/dto/todo/TodoPeriodResponse.java
+++ server/src/main/java/com/youtil/server/dto/todo/TodoPeriodResponse.java
@@ -1,5 +1,15 @@
 package com.youtil.server.dto.todo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class TodoPeriodResponse {
     private String minDate;
     private String maxDate;
