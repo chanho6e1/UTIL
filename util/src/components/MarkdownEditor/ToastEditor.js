217141e5feda702dd4f12cdaadafb4cba734805d
Dongju Kim <jook1356@gmail.com>
2023-02-02 16:32:34 +0900

    feat: fe/markdown

--- util/src/components/MarkdownEditor/ToastEditor.js
+++ util/src/components/MarkdownEditor/ToastEditor.js
@@ -1,22 +1,40 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import PostTag from './PostTag';
+import Button from '../UI/Button/Button';
 
 // Toast 에디터
 import { Editor } from '@toast-ui/react-editor';
 import './ToastEditor.css';
 import styles from './ToastEditor.module.css'
 
-export default function ToastEditor() {
+export default function ToastEditor(props) {
+
+
+  const departmentTagList = [
+    { name: "SSAFY", id: 0 },
+    { name: "삼성", id: 1 },
+    { name: "네이버", id: 2 },
+    { name: "카카오", id: 3 },
+    { name: "구글", id: 4 },
+    { name: "애플", id: 5 },
+  ];
+
+  const departmentTagNameList = departmentTagList.map((tag) => tag.name);
 
   return (
-    <div className={'editor-wrapper'}>
+    <div className={styles['editor-wrapper']}>
       <div className={styles['additional-info-wrapper']}>
-        <input type="text" className={styles['title-input']} placeholder="제목을 입력하세요." />
-
+        <div className={styles['header']}>
+          <input type="text" className={styles['title-input']} placeholder="제목을 입력하세요." />
+          <Button className={styles['button']}>글 작성</Button>
+        </div>
+        
+        <PostTag />
       </div>
       <Editor
         placeholder="내용을 입력해주세요."
         previewStyle="vertical" // 미리보기 스타일 지정
-        height="300px" // 에디터 창 높이
+        height="100%" // 에디터 창 높이
         initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
         toolbarItems={[
           // 툴바 옵션 설정
