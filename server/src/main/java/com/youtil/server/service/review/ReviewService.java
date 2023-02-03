9c5f3cff6ca0c13b790f7f5a3fb6e94510ab4ffc
“gms9424” <gms9424@naver.com>
2023-02-03 17:17:30 +0900

    feat: be/review_isMine

--- server/src/main/java/com/youtil/server/service/review/ReviewService.java
+++ server/src/main/java/com/youtil/server/service/review/ReviewService.java
@@ -1,5 +1,6 @@
 package com.youtil.server.service.review;
 
+import com.youtil.server.common.exception.ResourceForbiddenException;
 import com.youtil.server.common.exception.ResourceNotFoundException;
 import com.youtil.server.domain.goal.Goal;
 import com.youtil.server.domain.review.Review;
@@ -38,32 +39,54 @@ public class ReviewService {
         return review.getReviewId();
     }
 
-    public List<ReviewResponse> getReviewList(Long goalId, String criteria, int offset, int size) {
+    public List<ReviewResponse> getReviewList(Long goalId, String criteria, int offset, int size, Long userId) {
         Goal goal = goalRepository.findGoalByGoalId(goalId).orElseThrow(() -> new ResourceNotFoundException("Goal", "goalId", goalId));
 
 //        System.out.println(reviewQueryRepository.findReviewListByGoal(goalId, PageRequest.of(offset, size)).stream().map((review) -> new ReviewResponse(review)).collect(Collectors.toList()));
 
 //        return goalQueryRepository.findGoalListByUser(userId).stream().map((goal) -> new GoalResponse(goal)).collect(Collectors.toList());
 
+        validGoalUser(userId, goal.getUser().getUserId()); //나의 목표가 아니다
+
         return reviewQueryRepository.findReviewListByGoal(goalId, PageRequest.of(offset-1, size)).stream().map((review) -> new ReviewResponse(review)).collect(Collectors.toList());
     }
 
-    public ReviewResponse getReview(Long reviewId){
+    public ReviewResponse getReview(Long reviewId, Long userId){
         Review review =reviewRepository.findReviewByReviewId(reviewId).orElseThrow(() -> new ResourceNotFoundException("Review", "reviewId", reviewId));
+        Review myReview = reviewRepository.isMine(review.getReviewId(), userId).orElseThrow(() -> new ResourceNotFoundException("내가 만든 회고록이 아닙니다"));
 
         return new ReviewResponse(review);
     }
 
     @Transactional
-    public Long updateReview(Long reviewId, ReviewUpdateRequest request) {
+    public Long updateReview(Long reviewId, ReviewUpdateRequest request, Long userId) {
         Review review = reviewRepository.findReviewByReviewId(reviewId).orElseThrow(() -> new ResourceNotFoundException("Review", "reviewId", reviewId));
+
+        Review myReview = reviewRepository.isMine(review.getReviewId(), userId).orElseThrow(() -> new ResourceNotFoundException("내가 만든 회고록이 아닙니다"));
+
         review.update(request);
         return reviewId;
     }
 
     @Transactional
-    public Long deleteReview(Long reviewId) {
+    public Long deleteReview(Long reviewId, Long userId) {
+        Review review = reviewRepository.findReviewByReviewId(reviewId).orElseThrow(() -> new ResourceNotFoundException("Review", "reviewId", reviewId));
+
+        //내가 만든것만
+        Review myReview = reviewRepository.isMine(review.getReviewId(), userId).orElseThrow(() -> new ResourceNotFoundException("내가 만든 회고록이 아닙니다"));
+
         reviewRepository.deleteById(reviewId);
         return reviewId;
+    }
+
+
+    public void validGoalUser(Long currentUser, Long goalUser) {
+
+        if (currentUser == goalUser || currentUser.equals(goalUser)) {
+            return;
+        }
+        else {
+            throw new ResourceForbiddenException("본인이 작성한 회고록이 아닙니다");
+        }
     }
 }
