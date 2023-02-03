6db1a66341d44e61887c3e3d5eee9e9c922949be
Kihun SONG <kihunbuzz@naver.com>
2023-02-03 14:12:35 +0900

    feat:fe/follow api

---
+++ util/src/api/Post/deleteUnfollow.js
@@ -0,0 +1,25 @@
+import { API_BASE_URL, TOKEN } from "../../constants";
+import axios from "axios";
+
+export const deleteFollow = (toUserId) => {
+  console.log("unfollow", toUserId);
+  return axios({
+    method: "delete",
+    url: `${API_BASE_URL}/follows/${toUserId}`,
+    headers: {
+      Authorization: TOKEN(),
+    },
+  })
+    .then((res) => {
+      console.log("언팔로우 성공");
+      console.log(res);
+      return res.status;
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(TOKEN());
+      console.log("언팔로우 실패");
+    });
+};
+
+// /api/follows/{toUserId}
