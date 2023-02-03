80c23b77127a061852164db54b768dd0b7316467
박찬희 <gn05211@gmail.com>
2023-02-03 09:01:32 +0900

    chore:be/setting securityconfig Modify

---
+++ server/src/main/java/com/youtil/server/service/user/AuthService.java
@@ -0,0 +1,14 @@
+package com.youtil.server.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+//    public LoginResponse login(String provider, String code) {
+//
+//    }
+}
