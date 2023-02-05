fb51a5e3a6d2592f6a00feb1ae0fb1f4c8408f5f
sindorim5 <kihunbuzz@naver.com>
2023-02-05 22:34:32 +0900

    feat:fe/loading spinner, scroll listener

--- util/src/components/Main/Main.js
+++ util/src/components/Main/Main.js
@@ -20,7 +20,7 @@ const Main = (props) => {
   const userAuth = useSelector((state) => state.userAuthSlice.userAuth);
 
   const postData = {
-    content: [<MyUtil />, <Feed />, <UserPage id={2} />, <UserProfileChange />, <div>test1</div>],
+    content: [<MyUtil />, <Feed />, <div>test1</div>, <div>test1</div>, <div>test1</div>],
     dock: {
       logoContracted: (
         <img className={styles["logo-icon"]} src={logo} style={{ width: "96px", height: "auto" }} />
