692186ff4073f542aaea1ce2cab0ba35318e87d3
sindorim5 <kihunbuzz@naver.com>
2023-02-05 13:57:12 +0900

    feat:fe/nickname check

--- util/src/components/UserProfileChangePage/UserProfileChange.js
+++ util/src/components/UserProfileChangePage/UserProfileChange.js
@@ -1,54 +1,66 @@
 import { useEffect, useState } from "react";
 import classes from "./UserProfileChange.module.css";
 import UserProfileChangeCard from "../UI/UserProfileChangeCard/UserProfileChangeCard";
+import { getMyData } from "../../api/UserProfile/getMyData";
+import { getMyTags } from "../../api/UserProfile/getMyTags";
 
 const UserProfileChange = (props) => {
+  const [userData, setUserData] = useState("");
+  const [userTagList, setUserTagList] = useState(null);
+
+  useEffect(() => {
+    getMyData().then((res) => setUserData(() => res));
+    getMyTags().then((res) => {
+      setUserTagList(() => res.map((t) => t.tagName));
+    });
+  }, []);
+
   const submitUserProfileHandler = (userData) => {
     console.log("submithandler", userData);
     // 프사 업로드 axios
   };
 
-  const DUMMY_USERDATA = {
-    status: 200,
-    message: "유저 조회 성공",
-    data: {
-      userId: 4,
-      nickname: null,
-      email: "kihunbuzz@naver.com",
-      userName: "송기훈",
-      imageUrl:
-        "http://k.kakaocdn.net/dn/buIL20/btrWzWtdtbm/ZDcQAE4bkLbBGFXOflr63k/img_110x110.jpg",
-      department: null,
-      discription: null,
-    },
-  };
+  // {
+  //   "status": 200,
+  //   "message": "유저 정보 조회 성공",
+  //   "data": {
+  //     "userId": 4,
+  //     "nickname": "kihunSONG",
+  //     "email": "kihunbuzz@naver.com",
+  //     "userName": "송기훈",
+  //     "imageUrl": "http://k.kakaocdn.net/dn/buIL20/btrWzWtdtbm/ZDcQAE4bkLbBGFXOflr63k/img_110x110.jpg",
+  //     "department": "string",
+  //   }
+  // }
 
-  const DUMMY_TAGDATA = {
-    status: 200,
-    message: "유저별 관심 테그 조회 성공",
-    data: [
-      {
-        tagId: 10,
-        tagName: "aa",
-      },
-      {
-        tagId: 11,
-        tagName: "bb",
-      },
-    ],
-  };
+  // {
+  //   "status": 200,
+  //   "message": "나의 관심 테그 조회 성공",
+  //   "data": [
+  //     {
+  //       "tagId": 10,
+  //       "tagName": "aa"
+  //     },
+  //     {
+  //       "tagId": 11,
+  //       "tagName": "bb"
+  //     }
+  //   ]
+  // }
 
   return (
     <div>
-      <UserProfileChangeCard
-        imageUrl={DUMMY_USERDATA.data.imageUrl}
-        userName={DUMMY_USERDATA.data.userName}
-        nickname={DUMMY_USERDATA.data.nickname}
-        description={DUMMY_USERDATA.data.discription}
-        department={DUMMY_USERDATA.data.department}
-        tagList={DUMMY_TAGDATA.data}
-        onConfirm={submitUserProfileHandler}
-      />
+      {userData && userTagList && (
+        <UserProfileChangeCard
+          imageUrl={userData.imageUrl}
+          userName={userData.userName}
+          nickname={userData.nickname}
+          description={userData.description}
+          department={userData.department}
+          myTagList={userTagList}
+          onConfirm={submitUserProfileHandler}
+        />
+      )}
     </div>
   );
 };
