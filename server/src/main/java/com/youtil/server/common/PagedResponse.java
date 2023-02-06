408044b934995292871b2dbba5a2ab774105a2f1
“gms9424” <gms9424@naver.com>
2023-02-06 15:37:23 +0900

    feat: be/paging

--- server/src/main/java/com/youtil/server/common/PagedResponse.java
+++ server/src/main/java/com/youtil/server/common/PagedResponse.java
@@ -10,32 +10,40 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class PagedResponse<T> {
-    private List<T> data;
+    private List<T> content;
     private int page;
     private int size;
+    private long totalElements;
     private int totalPages;
     private boolean last;
-    private boolean hasNext;
-    private boolean hasPrevious;
 
-//    public PagedResponse(List<T> data, int page, int size, long totalElements, int totalPages, boolean last) {
-//        this.data = data;
-//        this.page = page; // ban
-//        this.size = size;
-//        this.totalElements = totalElements;
-//        this.totalPages = totalPages; // ban
-//        this.last = last;
-//    }
-//
-    public PagedResponse(List<T> data, int page, int size, int totalPages, boolean last, boolean hasNext, boolean hasPrevious) {
-        this.data = data;
+    public PagedResponse(List<T> content, int page, int size, long totalElements, int totalPages, boolean last) {
+        setContent(content);
         this.page = page; // ban
         this.size = size;
+        this.totalElements = totalElements;
         this.totalPages = totalPages; // ban
         this.last = last;
-        this.hasNext = hasNext;
-        this.hasPrevious = hasPrevious;
     }
 
-}
+    public PagedResponse(int page, int size, long totalElements, int totalPages, boolean last) {
+//        setContent(content);
+        this.page = page; // ban
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages; // ban
+        this.last = last;
+    }
 
+    public List<T> getContent() {
+        return content == null ? null : new ArrayList<>(content);
+    }
+
+    public final void setContent(List<T> content) {
+        if (content == null) {
+            this.content = null;
+        } else {
+            this.content = Collections.unmodifiableList(content);
+        }
+    }
+}
