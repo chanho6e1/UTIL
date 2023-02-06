0a787e5537f8ae620130d22c996843103b9251f2
DongJu <jook1356@gmail.com>
2023-02-06 23:19:41 +0900

    feat: fe/plan_card

--- util/src/components/Plan/PlanCard/PlanCardPlanCRUD.js
+++ util/src/components/Plan/PlanCard/PlanCardPlanCRUD.js
@@ -8,7 +8,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import 'dayjs/locale/ko';
-import styles from './PlanCardTodoCRUD.module.css'
+import styles from './PlanCardPlanCRUD.module.css'
 import { styled, createTheme, ThemeProvider } from '@mui/material';
 import Button from '../../UI/Button/Button';
 import { editTodoAPI } from '../../../api/Plan/editTodoAPI';
@@ -16,6 +16,12 @@ import { modifyPlanSliceActions } from '../../../redux/planSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { newTodoAPI } from '../../../api/Plan/newTodoAPI';
 import { recvTodoPeriodAPI } from '../../../api/Plan/recvTodoPeriodAPI';
+import { editPlanAPI } from '../../../api/Plan/editPlanAPI';
+import NotiDeliverer from '../../UI/StackNotification/NotiDeliverer';
+import warning from "../../../img/Warning.png"
+import { newPlanAPI } from '../../../api/Plan/newPlanAPI';
+import { delPlanAPI } from '../../../api/Plan/delPlanAPI';
+import FixedModal from '../../UI/FixedModal/FixedModal';
 
 
 
@@ -66,14 +72,111 @@ const PlanCardPlanCRUD = (props) => {
     }
 
     const submitHandler = () => {
-      
+      if (props.plan) {
+        const processing = {
+            title: title,
+            startDate: startDate,
+            endDate: endDate
+          }
+          editPlanAPI(props.plan.goalId, processing)
+          .then((res) => {
+              dispatch(modifyPlanSliceActions.responsePlans(JSON.stringify(res)))
+              props.modalHandler()
+          })
+          .catch((err) => {
+            console.log('PlanItem : editPlanAPI => ', err)
+            if (err.response.data[0].message == "목표 내용이 없습니다.") {
+                setNotiContent('목표의 타이틀은 필수 입력 사항입니다.')
+                setDoneNotiState(true)
+            }
+          })
+      } else {
+        newPlanAPI(startDate, endDate, title)
+            .catch((err) => {
+                // navigate('/login');
+                console.log('Plan : newPlanAPI => ', err)
+                if (startDate == null || endDate == null) {
+                    setNotiContent('시작일과 마감일은 필수 입력 사항입니다.')
+                    setDoneNotiState(true)
+                }
+                if (err.response.data[0].message == "목표 내용이 없습니다.") {
+                    setNotiContent('목표의 타이틀은 필수 입력 사항입니다.')
+                    setDoneNotiState(true)
+                }
+            })
+            .then((res) => {
+                dispatch(modifyPlanSliceActions.responsePlans(JSON.stringify(res)))
+                props.modalHandler()
+
+                
+            })
+      }
     }
+
+    const [doneNotiState, setDoneNotiState] = useState(false)
+    const [notiContent, setNotiContent] = useState()
+    const [askDeleteState, setAskDeleteState] = useState(false)
+
+
+    const ErrorMessage = (props) => {
+        return (
+            <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center'}}>
+                <img style={{width:'40px', height:'40px', marginRight:'12px'}} src={warning} />
+                <div>
+                    <p style={{lineHeight: '40%'}}>{notiContent}</p>
+                    {/* <p style={{lineHeight: '40%'}}>날짜 범위를 미만 & 초과할 수 없습니다.</p> */}
+                </div>
+            </div>
+        )
+    }
+
+    const deletePlan = () => {
+        delPlanAPI(props.plan.goalId)
+        .then((res) => {
+          dispatch(modifyPlanSliceActions.responsePlans(JSON.stringify(res)))
+        })
+        .catch((err) => {
+          console.log('PlanItem : delPlanAPI => ', err)
+        })
+    
+    }
+
+    const AskDeleteForm = (props) => {
+        return (
+          <div style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+            <img style={{width:'140px', height:'auto', marginBottom: '12px'}} src={warning} />
+            <div>
+              <p style={{lineHeight: '40%'}}>삭제 시 복구할 수 없습니다.</p>
+              <p style={{lineHeight: '40%'}}>정말로 삭제 하시겠습니까?</p>
+            </div>
+            <div style={{width:'100%', display:'flex', justifyContent:'space-evenly', marginTop:'12px'}}>
+              <Button className={styles['modal-button']} onClick={props.modalHandler}>취소</Button>
+              <Button className={`${styles['modal-button']} ${styles['modal-delete']}`} onClick={() => {deletePlan(); props.modalHandler()}}>삭제</Button>
+            </div>
+          </div>
+        )
+      }
+    
+
+    
+
+    const deleteButton = (
+        <div onClick={() => {setAskDeleteState(true)}} className={styles['delete-button']}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
+                <path d="M6.854 7.146a.5.5 0 1 0-.708.708L7.293 9l-1.147 1.146a.5.5 0 0 0 .708.708L8 9.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 9l1.147-1.146a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146z"/>
+                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+            </svg>
+        </div>
+    )
 
 
     return (
         <div className={styles['form-wrapper']}>
+            <FixedModal modalState={askDeleteState} stateHandler={setAskDeleteState} content={<AskDeleteForm />} width={'340px'} height={'310px'} />
+            {doneNotiState && <NotiDeliverer content={<ErrorMessage />} stateHandler={setDoneNotiState} duration={5000} width={400} />}
             <div className={styles['title-wrapper']}>
-            {props.plan ? '목표 수정' : '목표 생성'}
+                {props.plan ? '목표 수정' : '목표 생성'}
+                {props.plan && deleteButton}
             </div>
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
                 <ThemeProvider theme={customTheme}>
@@ -83,6 +186,7 @@ const PlanCardPlanCRUD = (props) => {
                         <MobileDatePicker
                             label="시작일"
                             value={startDate}
+                            maxDate={endDate}
                             onChange={startDateChangeHandler}
                             renderInput={(params) => <TextField {...params} sx={{width:'100%', paddingBottom:'24px'}} />}
                         />
@@ -91,6 +195,7 @@ const PlanCardPlanCRUD = (props) => {
                         <MobileDatePicker
                             label="마감일"
                             value={endDate}
+                            minDate={startDate}
                             onChange={endDateChangeHandler}
                             renderInput={(params) => <TextField {...params} sx={{width:'100%', paddingBottom:'24px'}} />}
                         />
--- util/src/components/Plan/PlanCard/PlanCardTodoCRUD.js
+++ util/src/components/Plan/PlanCard/PlanCardTodoCRUD.js
@@ -16,6 +16,10 @@ import { modifyPlanSliceActions } from '../../../redux/planSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { newTodoAPI } from '../../../api/Plan/newTodoAPI';
 import { recvTodoPeriodAPI } from '../../../api/Plan/recvTodoPeriodAPI';
+import NotiDeliverer from '../../UI/StackNotification/NotiDeliverer';
+import warning from "../../../img/Warning.png"
+import { delTodoAPI } from '../../../api/Plan/delTodoAPI';
+import FixedModal from '../../UI/FixedModal/FixedModal';
 
 
 
@@ -87,6 +91,11 @@ const PlanCardTodoCRUD = (props) => {
             })
             .catch((err) => {
                 console.log('PlanCardTodoCRUD : editTodoAPI => ', err)
+                if (err.response.data[0].message == "제목이 없습니다.") {
+                    setNotiContent('TODO의 타이틀은 필수 입력 사항입니다.')
+                    setDoneNotiState(true)
+                }
+                
             })
 
         } else {
@@ -116,19 +125,80 @@ const PlanCardTodoCRUD = (props) => {
             .catch((err) => {
                 console.log('PlanCardTodoCRUD : newTodoAPI => ', err)
     
-                // if (err.response.data.message == "todo 날짜가 목표 범위 벗어남") {
-                //     setNotiState(true)
-                // }
+                if (err.response.data[0].message == "제목이 없습니다.") {
+                    setNotiContent('TODO의 타이틀은 필수 입력 사항입니다.')
+                    setDoneNotiState(true)
+                }
                 
             })
         }
     }
 
 
+    const [doneNotiState, setDoneNotiState] = useState(false)
+    const [notiContent, setNotiContent] = useState()
+    const [askDeleteState, setAskDeleteState] = useState(false)
+
+    const ErrorMessage = (props) => {
+        return (
+            <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center'}}>
+                <img style={{width:'40px', height:'40px', marginRight:'12px'}} src={warning} />
+                <div>
+                    <p style={{lineHeight: '40%'}}>{notiContent}</p>
+                    {/* <p style={{lineHeight: '40%'}}>날짜 범위를 미만 & 초과할 수 없습니다.</p> */}
+                </div>
+            </div>
+        )
+    }
+
+    const deleteTodo = () => {
+        delTodoAPI(props.todo.todoId, props.plan.goalId)
+        .then((res) => {
+            const proccessing = {
+                goalId: props.plan.goalId,
+                data: res
+            }
+            dispatch(modifyPlanSliceActions.responseTodos(JSON.stringify(proccessing)))
+        })
+    }
+
+
+    const AskDeleteForm = (props) => {
+        return (
+          <div style={{width:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+            <img style={{width:'140px', height:'auto', marginBottom: '12px'}} src={warning} />
+            <div>
+              <p style={{lineHeight: '40%'}}>삭제 시 복구할 수 없습니다.</p>
+              <p style={{lineHeight: '40%'}}>정말로 삭제 하시겠습니까?</p>
+            </div>
+            <div style={{width:'100%', display:'flex', justifyContent:'space-evenly', marginTop:'12px'}}>
+              <Button className={styles['modal-button']} onClick={props.modalHandler}>취소</Button>
+              <Button className={`${styles['modal-button']} ${styles['modal-delete']}`} onClick={() => {deleteTodo(); props.modalHandler()}}>삭제</Button>
+            </div>
+          </div>
+        )
+      }
+    
+
+    const deleteButton = (
+        <div onClick={() => {setAskDeleteState(true)}} className={styles['delete-button']}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
+                <path d="M6.854 7.146a.5.5 0 1 0-.708.708L7.293 9l-1.147 1.146a.5.5 0 0 0 .708.708L8 9.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 9l1.147-1.146a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146z"/>
+                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+            </svg>
+        </div>
+    )
+
+    
+
+
     return (
         <div className={styles['form-wrapper']}>
+            <FixedModal modalState={askDeleteState} stateHandler={setAskDeleteState} content={<AskDeleteForm />} width={'340px'} height={'310px'} />
+            {doneNotiState && <NotiDeliverer content={<ErrorMessage />} stateHandler={setDoneNotiState} duration={5000} width={400} />}
             <div className={styles['title-wrapper']}>
             {props.todo ? 'TODO 수정' : 'TODO 생성'}
+            {props.todo && deleteButton}
             </div>
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
                 <ThemeProvider theme={customTheme}>
