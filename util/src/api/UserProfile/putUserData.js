1af8dc7529c7b31bf7afd0c2be266cc9b3a01044
Kihun SONG <kihunbuzz@naver.com>
2023-02-07 14:13:36 +0900

    feat:fe/userprofilechange axios

---
+++ util/src/api/UserProfile/putUserData.js
@@ -0,0 +1,21 @@
+import { API_BASE_URL, TOKEN } from "../../constants";
+import axios from "axios";
+
+export const putUserData = (userData) => {
+  return axios({
+    method: "put",
+    url: `${API_BASE_URL}/user`,
+    headers: {
+      Authorization: TOKEN(),
+    },
+    data: userData,
+  })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(TOKEN());
+      console.log("유저 정보 업데이트 실패");
+    });
+};
