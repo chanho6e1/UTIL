b11c989107f4ddecb65e573312622406d785a003
sindorim5 <kihunbuzz@naver.com>
2023-02-07 23:47:32 +0900

    feat:fe/searchbar with search page

---
+++ util/src/components/Feed/SearchFeed.js
@@ -0,0 +1,114 @@
+import FeedCardItem from "../UI/FeedCard/FeedCardItem";
+import classes from "./Feed.module.css";
+import { getPosts } from "../../api/Post/getPosts";
+import { useState, useEffect, useRef, Fragment } from "react";
+import Loading from "../UI/Loading/Loading";
+import { getPostSearch } from "../../api/Post/getPostSearch";
+import { getPostByTagName } from "../../api/Post/getPostByTagName";
+
+const feedCardItemList = (postList) => {
+  return postList?.map((post) => {
+    return (
+      <FeedCardItem
+        id={post.postId}
+        key={post.postId}
+        thumbnail={post.thumbnail}
+        title={post.title}
+        contents={post.contents}
+        likeStatusSize={post.likeStatusSize}
+        likeStatus={post.likeStatus}
+        bookmarkStatus={post.bookmarkStatus}
+        profileImg={post.writerInfo.profileImg}
+        nickname={post.writerInfo.nickname}
+        createdDate={post.createdDate}
+      />
+    );
+  });
+};
+
+const SearchFeed = (props) => {
+  console.log("SSAFY SF", props);
+  const [feedList, setFeedList] = useState([]);
+  const criteria = ["date", "view", "like"];
+  const [offset, setOffset] = useState(1);
+  const size = 10;
+
+  const searchRef = useRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (props.searchInput !== null && props.searchInput !== "") {
+      console.log("SSAFY UE", props);
+      setIsLoading(true);
+      if (props.api === 0) {
+        getPostSearch(criteria[0], offset, size, props.searchInput).then((res) => {
+          if (res.content.length !== 0) {
+            setFeedList(() => res.content);
+          }
+          setIsLoading(false);
+        });
+      } else {
+        getPostByTagName(criteria[0], offset, size, props.searchInput).then((res) => {
+          if (res.content.length !== 0) {
+            setFeedList(() => res.content);
+          }
+          setIsLoading(false);
+        });
+      }
+    }
+  }, [props.searchInput, props.api]);
+
+  const fetchMoreData = () => {
+    setIsLoading(true);
+    if (props.api === 0) {
+      getPostSearch(criteria[0], offset + 1, size, props.searchInput).then((res) => {
+        setFeedList((prevState) => [...prevState, ...res.content]);
+        setOffset((prevState) => prevState + 1);
+        setIsLoading(false);
+      });
+    } else {
+      getPostByTagName(criteria[0], offset + 1, size, props.searchInput).then((res) => {
+        setFeedList((prevState) => [...prevState, ...res.content]);
+        setOffset((prevState) => prevState + 1);
+        setIsLoading(false);
+      });
+    }
+  };
+
+  // scroll event handler
+  const handleScroll = () => {
+    const scrollHeight = searchRef.current.scrollHeight;
+    const scrollTop = searchRef.current.scrollTop;
+    const clientHeight = searchRef.current.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10 && isLoading === false) {
+      // 페이지 끝에 도달하면 추가 데이터를 받아온다
+      fetchMoreData();
+    }
+  };
+
+  useEffect(() => {
+    // scroll event listener 등록
+    if (searchRef.current !== null) {
+      searchRef.current.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      // scroll event listener 해제
+      if (searchRef.current !== null) {
+        searchRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  });
+
+  return (
+    <Fragment>
+      <div className={classes.feed} ref={searchRef}>
+        {<ul>{feedCardItemList(feedList)}</ul>}
+      </div>
+      <div>{isLoading && <div className={classes.loading}>{Loading()}</div>}</div>
+    </Fragment>
+  );
+};
+
+export default SearchFeed;
