0f4eb0992ebf189cbadcf5329ef9444469f5225e
Kihun SONG <kihunbuzz@naver.com>
2023-02-08 18:05:10 +0900

    fix:fe/merge error fix

--- util/src/components/UserPage/UserPage.js
+++ util/src/components/UserPage/UserPage.js
@@ -43,9 +43,7 @@ const postCardItemList = (postList) => {
 };
 
 const UserPage = (props) => {
-
-
-  const containerRef = useRef()
+  const containerRef = useRef();
   const [postList, setPostList] = useState(null);
   const [userData, setUserData] = useState([]);
   const [myData, setMyData] = useState([]);
@@ -84,18 +82,15 @@ const UserPage = (props) => {
     // });
   };
 
-
-  const [fetchStart, setFetchStart] = useState(false)
+  const [fetchStart, setFetchStart] = useState(false);
 
   useEffect(() => {
     if (fetchStart === true) {
-      fetchUserPostDataMobile(criteriaIdx, offset, size)
+      fetchUserPostDataMobile(criteriaIdx, offset, size);
     }
-    
-  }, [fetchStart])
+  }, [fetchStart]);
 
   const fetchUserPostDataMobile = (criteriaIdx, page, size) => {
-    
     setIsLoading(true);
 
     getMyPosts(criteria[criteriaIdx], page + 1, size).then((res) => {
@@ -104,11 +99,10 @@ const UserPage = (props) => {
       // setIsLoading(false)
       setTimeout(() => {
         setIsLoading(false);
-        setFetchStart(() => false)
+        setFetchStart(() => false);
       }, 500);
     });
 
-    
     // getUserPosts(props.id, criteria[criteriaIdx], page, size).then((res) => {
     //   setPostList(() => res.content);
     //   setOffset(() => page);
@@ -117,7 +111,7 @@ const UserPage = (props) => {
     //   }, 500);
     // });
   };
-  
+
   // 초기 데이터
   useEffect(() => {
     // Post API
@@ -137,13 +131,13 @@ const UserPage = (props) => {
     // My Data API
     getMyData().then((res) => {
       setMyData(() => res);
-      console.log('ssafy me', res)
+      console.log("ssafy me", res);
     });
 
     // User Data API
     getUserData(props.id).then((res) => {
       setUserData(() => res);
-      console.log('ssafy user', res)
+      console.log("ssafy user", res);
     });
 
     // Tag Data API
@@ -151,7 +145,6 @@ const UserPage = (props) => {
       setUserTagList(() => res);
     });
   }, []);
-
 
   // Follow Data API
   useEffect(() => {
@@ -223,43 +216,35 @@ const UserPage = (props) => {
     fetchUserPostData(criteriaIdx, page, size);
   };
 
+  const wrapperDiv = useRef();
 
+  // scroll event handler
+  const handleScroll = () => {
+    const scrollHeight = containerRef.current.scrollHeight;
+    const scrollTop = containerRef.current.scrollTop;
+    const clientHeight = containerRef.current.clientHeight;
+    console.log("SSAFY st", scrollTop, "ch", clientHeight, "sh", scrollHeight);
+    if (scrollTop + clientHeight >= scrollHeight - 1 && isLoading === false) {
+      if (offset < totalPage) {
+        setFetchStart(() => true);
+      }
 
-  const wrapperDiv = useRef()
+      // 페이지 끝에 도달하면 추가 데이터를 받아온다
+    }
+  };
 
-    // scroll event handler
-    const handleScroll = () => {
-      const scrollHeight = containerRef.current.scrollHeight;
-      const scrollTop = containerRef.current.scrollTop;
-      const clientHeight = containerRef.current.clientHeight;
-      console.log("SSAFY st", scrollTop, "ch", clientHeight, "sh", scrollHeight)
-      if (scrollTop + clientHeight >= scrollHeight - 1 && isLoading === false) {
-
-        if (offset < totalPage) {
-          setFetchStart(() => true)
-        }
-        
-        // 페이지 끝에 도달하면 추가 데이터를 받아온다
-        
-        
+  useEffect(() => {
+    // scroll event listener 등록
+    if (containerRef.current !== null) {
+      containerRef.current.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      // scroll event listener 해제
+      if (containerRef.current !== null) {
+        containerRef.current.removeEventListener("scroll", handleScroll);
       }
     };
-  
-    useEffect(() => {
-      // scroll event listener 등록
-      if (containerRef.current !== null) {
-        containerRef.current.addEventListener("scroll", handleScroll);
-      }
-      return () => {
-        // scroll event listener 해제
-        if (containerRef.current !== null) {
-          containerRef.current.removeEventListener("scroll", handleScroll);
-        }
-      };
-    });
-
-
-
+  });
 
   const userPageUpper = (
     <div className={classes[`user-page-upper`]}>
@@ -305,24 +290,18 @@ const UserPage = (props) => {
 
   return (
     <div ref={wrapperDiv} className={classes[`user-page`]}>
-
-        
-        <div ref={containerRef} className={classes[`postcard-container`]}>
-          <div  className={classes[`postcard-inner`]}>
-            {userPageUpper}
-            <div className={classes['line']} />
-            {postCardContainer(postList)}
-          </div>
-          
-          <div className={classes['plan-card-wrapper']}>
-            <PlanCard />
-          </div>
+      <div ref={containerRef} className={classes[`postcard-container`]}>
+        <div className={classes[`postcard-inner`]}>
+          {userPageUpper}
+          <div className={classes["line"]} />
+          {postCardContainer(postList)}
         </div>
 
         <div className={classes["plan-card-wrapper"]}>
           <PlanCard />
         </div>
       </div>
+
       <div className={classes[`pagination`]}>
         <Pagination count={totalPage} onChange={pageChangeHandler} />
       </div>
