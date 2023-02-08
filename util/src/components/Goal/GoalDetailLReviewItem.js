a06a0f20f39d7ac92391fde0c161e4652c894544
Choi-Jihun <totolo312@naver.com>
2023-02-08 13:42:08 +0900

    feat/fe/Detail Review

--- util/src/components/Goal/GoalDetailLReviewItem.js
+++ util/src/components/Goal/GoalDetailLReviewItem.js
@@ -1,9 +1,13 @@
 import classes from "../Goal/GoalDetail.module.css";
 
 const GoalDetailLReviewItem = (props) => {
+
   return (
     <li className={classes["goal-detail-reviews-item"]}>
-      <div className={classes["goal-detail-reviews-item-date"]}>{props.review.createdDate}</div>
+      <div className={classes['goal-detail-reviews-item-title']}>
+        <div className={classes["goal-detail-reviews-item-date"]}>{props.review.createdDate}</div>
+        <div className={classes['goal-detail-reviews-item-link']}>수정</div>
+      </div>
       <div className={classes["goal-detail-reviews-item-content"]}>{props.review.content}</div>
     </li>
   );
