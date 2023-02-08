b0206725ce402ed9b109a8651bbe16e1d631c9a3
Kihun SONG <kihunbuzz@naver.com>
2023-02-08 15:21:53 +0900

    style:fe/button disabled props, hover enabled css

--- util/src/components/UI/Button/Button.js
+++ util/src/components/UI/Button/Button.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Button.module.css'
+import styles from "./Button.module.css";
 
 const Button = (props) => {
-  
   return (
-    <button className={`${styles.button} ${props.className} `} type={props.type || 'button'} onClick={props.onClick}>
+    <button
+      className={`${styles.button} ${props.className} `}
+      type={props.type || "button"}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
