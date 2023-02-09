b76fbef806f0e99f900b5330581da6d5af496a30
ai06010 <ai06010@naver.com>
2023-02-09 17:44:41 +0900

    fix: be/tag 이미지 변경

--- server/src/main/java/com/youtil/server/service/tag/TagService.java
+++ server/src/main/java/com/youtil/server/service/tag/TagService.java
@@ -183,7 +183,7 @@ public class TagService {
                 page.getTotalPages(), page.isLast());
     }
 
-    public PagedResponse<PostResponse> findUserListByMyTag(Long userId, int offset, int size) { // 나의 관심 테그로 포스트 조회
+    public PagedResponse<UserResponse> findUserListByMyTag(Long userId, int offset, int size) { // 나의 관심 테그로 포스트 조회
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
         Page<User> page = tagQueryRepository.findUserListByMyTag(userId, PageRequest.of(offset-1, size));
         List<UserResponse> responses = page.stream().map((u)-> new UserResponse(u)).collect(Collectors.toList());
