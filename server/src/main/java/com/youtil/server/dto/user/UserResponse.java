476114b2e05129ceb41fc12634acbab64a72a62f
ai06010 <ai06010@naver.com>
2023-02-10 12:44:24 +0900

    fix: be/tag 별 유저리스트 response 변경

--- server/src/main/java/com/youtil/server/dto/user/UserResponse.java
+++ server/src/main/java/com/youtil/server/dto/user/UserResponse.java
@@ -27,13 +27,6 @@ public class UserResponse {
 
     private String discription;
 
-    public UserResponse(User u) {
-        this.userId = u.getUserId();
-        this.userName = u.getUserName();
-        this.nickname = u.getNickName();
-        this.imageUrl = "https://utilbucket.s3.ap-northeast-2.amazonaws.com/static/user/" + u.getImageUrl();
-    }
-
     public static UserResponse from(User user) {
         String imgUrl = "https://utilbucket.s3.ap-northeast-2.amazonaws.com/static/user/" + user.getImageUrl();
         return new UserResponse(user.getUserId(), user.getNickName(), user.getEmail(), user.getUserName(), imgUrl, user.getDepartment(), user.getDiscription());
