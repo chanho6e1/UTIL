f284a0e33ba4a16422b7d9dc4633056f04bbdffb
sindorim5 <kihunbuzz@naver.com>
2023-02-11 17:57:21 +0900

    feat:fe/followcard with modal

---
+++ util/src/components/UI/FollowModal/FollowCard.js
@@ -0,0 +1,96 @@
+import classes from "./FollowCard.module.css";
+import { Avatar } from "@mui/material";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import { useState, useEffect } from "react";
+import { getIsFollowing } from "../../../api/Post/getIsFollowing";
+import { postFollow } from "../../../api/Post/postFollow";
+import { deleteFollow } from "../../../api/Post/deleteUnfollow";
+
+const FollowCard = (props) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    getIsFollowing(props.userData.userId).then((res) => {
+      setIsFollowing(() => res);
+    });
+  }, []);
+
+  const profileOnClickHandler = () => {
+    // 클릭 시 유저 페이지로 이동
+    console.log("profile", props.userData.userId);
+  };
+
+  const followBtnHandler = () => {
+    // 현재 팔로우 중이면 언팔
+    if (isFollowing) {
+      deleteFollow(props.userData.userId).then((res) => {
+        if (res === 200) {
+          setIsFollowing((prevState) => !prevState);
+        }
+      });
+    } else {
+      postFollow(props.userData.userId).then((res) => {
+        if (res === 200) {
+          setIsFollowing((prevState) => !prevState);
+        }
+      });
+    }
+  };
+
+  const followBtn = (isFollowing) => {
+    if (isFollowing) {
+      return (
+        <Button
+          className={`${classes[`follow-btn-true`]} ${classes[`button`]}`}
+          onClick={followBtnHandler}
+        >
+          팔로잉
+        </Button>
+      );
+    } else {
+      return (
+        <Button
+          className={`${classes[`follow-btn-false`]} ${classes[`button`]}`}
+          onClick={followBtnHandler}
+        >
+          팔로우
+        </Button>
+      );
+    }
+  };
+
+  return (
+    <Card className={classes.card}>
+      <div className={classes[`followcard-content`]}>
+        <div className={classes[`left-content`]}>
+          <div className={classes[`avatar-wrapper`]}>
+            <Avatar
+              src={props.userData.imageUrl}
+              sx={{
+                width: "10vw",
+                height: "10vw",
+                maxWidth: "80px",
+                maxHeight: "80px",
+                border: "1px solid lightgray",
+                objectFit: "scale-down",
+              }}
+              onClick={profileOnClickHandler}
+            />
+          </div>
+          <div className={classes[`nickname-username`]}>
+            <div className={classes.nickname} onClick={profileOnClickHandler}>
+              {props.userData.nickName}
+            </div>
+            <div className={classes.username} onClick={profileOnClickHandler}>
+              {props.userData.userName}
+            </div>
+          </div>
+        </div>
+        <div className={classes[`btn-wrapper`]}>{followBtn(isFollowing)}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default FollowCard;
---
+++ util/src/components/UI/FollowModal/FollowModal.js
@@ -0,0 +1,45 @@
+import classes from "./FollowModal.module.css";
+import { Fragment, useEffect, useState } from "react";
+import FollowCard from "./FollowCard";
+import { IconButton } from "@mui/material";
+import CloseIcon from "../../../img/Close40.svg";
+
+const FollowCardList = (followList) => {
+  return followList?.map((user) => {
+    return <FollowCard id={user.userId} key={`follow-card-${user.userId}`} userData={user} />;
+  });
+};
+
+const FollowModal = (props) => {
+  const [followData, setFollowData] = useState(props.followData);
+
+  useEffect(() => {
+    if (props.followData) {
+      setFollowData(() => [...props.followData]);
+    }
+  }, []);
+
+  return (
+    <div className={classes[`follow-modal`]}>
+      <div className={classes[`follow-modal-upper`]}>
+        <div className={classes[`follow-text`]}>{props.title}</div>
+        <IconButton
+          onClick={() => {
+            props.onFollowModalClose();
+            props.modalHandler();
+          }}
+          style={{
+            padding: 0,
+          }}
+        >
+          <img src={CloseIcon} />
+        </IconButton>
+      </div>
+      <div className={classes[`follow-modal-lower`]}>
+        <div className={classes[`follow-modal-contents`]}>{FollowCardList(followData)}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FollowModal;
