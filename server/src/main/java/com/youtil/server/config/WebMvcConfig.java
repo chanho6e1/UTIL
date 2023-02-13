fe110b0c4cb3cccfc664cdf5cf3c3fceb6326de7
박찬희 <gn05211@gmail.com>
2023-02-14 00:00:28 +0900

    chore: be/setting SecurityConfig origin 설정 추가

--- server/src/main/java/com/youtil/server/config/WebMvcConfig.java
+++ server/src/main/java/com/youtil/server/config/WebMvcConfig.java
@@ -5,11 +5,15 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,7 +24,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://i8d210.p.ssafy.io")
-                .allowCredentials(true)
 //                .allowedOrigins("*")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
