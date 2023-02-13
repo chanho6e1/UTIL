99962c207bc2ae14533a3b81cda5cc5b4009ee33
Dongju Kim <jook1356@gmail.com>
2023-02-13 13:12:59 +0900

    feat: fe/css

--- util/src/components/UI/TextArea/TextArea.js
+++ util/src/components/UI/TextArea/TextArea.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import styles from './TextArea.module.css'
+import './ToastEditor.css';
+
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
+import prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+
 
 const TextArea = (props) => {
   
