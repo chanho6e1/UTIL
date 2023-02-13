e6852d54a8aa6b1bbad48c47684760b7edd88b21
Choi-Jihun <totolo312@naver.com>
2023-02-13 15:01:34 +0900

    feat/fe/Detail css

--- util/src/components/UI/PlanList/PlanList.js
+++ util/src/components/UI/PlanList/PlanList.js
@@ -8,13 +8,15 @@ const PlanList = (props) => {
   const navigate = useNavigate()
   
   return (
-    <Card className={classes['plan']}>
-      <div  onClick={() => navigate(`/index/goal/${props.plan.goalId}`)}>
-        <div className={classes['plan-title']}>{props.plan.title}</div>
-        <div className={classes['plan-date']}>{props.plan.startDate} ~ {props.plan.endDate}</div>
-      </div>
-      <div className={classes['plan-state']}>
-        <PlanState state={props.plan.state} startDate={props.plan.startDate} endDate={props.plan.endDate} />
+    <Card>
+      <div onClick={() => navigate(`/index/goal/${props.plan.goalId}`)} className={classes['plan']}>
+        <div>
+          <div className={classes['plan-title']}>{props.plan.title}</div>
+          <div className={classes['plan-date']}>{props.plan.startDate} ~ {props.plan.endDate}</div>
+        </div>
+        <div className={classes['plan-state']}>
+          <PlanState state={props.plan.state} startDate={props.plan.startDate} endDate={props.plan.endDate} />
+        </div>
       </div>
     </Card>
   )
