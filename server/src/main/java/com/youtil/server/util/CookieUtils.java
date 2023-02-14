81e9c55d3259711a945d24d9877dec45676ebd57
박찬희 <gn05211@gmail.com>
2023-02-14 19:34:44 +0900

    chore: be/setting CookieUtils setSecure 주석

--- server/src/main/java/com/youtil/server/util/CookieUtils.java
+++ server/src/main/java/com/youtil/server/util/CookieUtils.java
@@ -30,7 +30,7 @@ public class CookieUtils {
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setMaxAge(maxAge);
-        cookie.setSecure(true);
+//        cookie.setSecure(true);
         response.addCookie(cookie);
 //        ResponseCookie cookie = ResponseCookie.from(name, value)
 //                .path("/")
