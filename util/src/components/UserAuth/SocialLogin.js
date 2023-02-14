e4cb35d92bd0681a9ee9b3559e4887b118f49cf5
Dongju Kim <jook1356@gmail.com>
2023-02-14 17:55:49 +0900

    feat: fe/login

--- util/src/components/UserAuth/SocialLogin.js
+++ util/src/components/UserAuth/SocialLogin.js
@@ -1,18 +1,48 @@
 import React from "react";
 import SocialLoginModule from "./SocialLoginModule";
 import styles from "./SocialLogin.module.css";
+import illust1 from "../../img/5172479.png";
+import logo from "../../img/util-logo-black.png"
 
 const SocialLogin = (props) => {
   return (
     <div className={styles["social-login"]}>
-      <div className={styles["background"]}>util</div>
-      <div className={styles["content"]}>
-        <div className={styles["box"]}>
-          <div className={styles["signin-label"]}>Sign In</div>
-          <div className={styles["welcome-label"]}>환영합니다!</div>
-          <SocialLoginModule />
+
+      
+
+      <div className={styles["login-wrapper"]}>
+
+        <div className={styles["header"]}>
+          <div className={styles["logo-wrapper"]}>
+                <img className={styles['logo-icon']} style={{ width: "auto", height: "80px", marginRight: '-15px' }} src={logo} />
+                <div className={styles["logo-title"]}>til</div>
+          </div>
         </div>
+
+        <img className={styles['mobile-image']} style={{ width: "100vw", height: "auto", marginTop:'-20vw', maxWidth:'500px'}} src={illust1} />
+
+        <div className={styles["body"]}>
+
+          
+          
+          <div className={styles['ment-wrapper']}>
+            <div className={styles['ment-1']}>
+            <div className={styles["line"]} />
+              <b>전문가</b>로 성장하는
+            </div>
+            <div className={styles['ment-2']}>
+              가장 <b>정확한</b> 길
+            </div>
+            <div className={styles['sub-ment-1']}>“누군가가 원하는 것을 얘기 하기만 하면 되는 프로그래밍 언어를 원한다고 말하면 그에게 막대 사탕을 주도록 하라.”</div>
+          </div>
+        </div>
+        
+        <SocialLoginModule />
+        <div/>
       </div>
+
+      <img className={styles['pc-image']} style={{ width: "60vw", height: "auto", minWidth: "500px"}} src={illust1} />
+      <a href="https://www.freepik.com/free-vector/programming-concept-illustration_7118756.htm#query=programmer&position=19&from_view=keyword&track=sph">Image by storyset</a> on Freepik
     </div>
   );
 };
